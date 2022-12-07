a154f68a05baed353d3861cf568de38a88cce7eb
ProKiii <prokiiifo@gmail.com>
2022-12-07 01:36:41 +0000

    owo homepageD com css

--- app.js
+++ app.js
@@ -48,13 +48,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-<<<<<<< Updated upstream
-app.use(express.static(__dirname + "/public"));
-=======
 
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> Stashed changes
 
 app.use('/', indexRouter);
 app.use('/homepage', homepageRouter);
