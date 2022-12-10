90274a5dfdf431d15409a4e5001a9575a779909f
ProKiii <prokiiifo@gmail.com>
2022-12-10 00:57:18 +0000

    testes relatorios

--- routes/homepage.js
+++ routes/homepage.js
@@ -33,7 +33,6 @@ router.get('/D', function (req, res, next) {
     }
 });
 
-
 async function buscaAlunosDaTurmaX(turma_x) {
     let database = new Database("base_de_dados.sqlite3", sqlite3.OPEN_READONLY);
     return new Promise((resolve, reject) => {
@@ -43,25 +42,91 @@ async function buscaAlunosDaTurmaX(turma_x) {
                 database.close();
                 reject(err);
             }
-            console.log(alunos_list)
+            //console.log(alunos_list)
             database.close();
             resolve(alunos_list);
         });
     });
-
-
 };
 
-router.get('/D/alunos/:turma', async function (req, res, next) {
+async function buscaRelatoriosDoAlunoX(aluno_x) {
+    let database = new Database("base_de_dados.sqlite3", sqlite3.OPEN_READONLY);
+    return new Promise((resolve, reject) => {
+        database.all("SELECT * FROM relatorios WHERE id_aluno = ?", [aluno_x], function (err, relatorios_list) {
+            if (err) {
+                console.error("Erro ao procurar relatórios", err);
+                database.close();
+                reject(err);
+            }
+            //console.log(relatorios_list);
+            database.close();
+            resolve(relatorios_list);
+        });
+    });
+}
+
+async function buscaAlunoX(aluno_x) {
+    let database = new Database("base_de_dados.sqlite3", sqlite3.OPEN_READONLY);
+    return new Promise((resolve, reject) => {
+        database.all("SELECT * FROM alunos WHERE id = ?", [aluno_x], function (err, aluno_list) {
+            if (err) {
+                console.error("Erro ao procurar aluno", err);
+                database.close();
+                reject(err);
+            }
+            //console.log(aluno_list);
+            database.close();
+            resolve(aluno_list);
+        });
+    });
+}
+
+
+router.get('/D/:turma/alunos', async function (req, res, next) {
     if (req.session.cookie.secure == true && req.session.cookie.type == 'docente') {
         buscaAlunosDaTurmaX(req.params.turma).then(alunos => {
-            console.log(alunos);
-            res.render('alunos', { title: 'Alunos', alunos: alunos });
+            //console.log(alunos);
+            res.render('alunos', { title: 'Alunos', turma: req.params.turma, alunos: alunos });
         });
 
     } else {
         res.redirect('/');
     }
 })
+
+router.get('/D/:turma/:aluno/relatorios', async function(req, res, next) {
+    if (req.session.cookie.secure == true && req.session.cookie.type == 'docente') {
+        let aluno_nome = '';
+
+        await buscaAlunoX(req.params.aluno).then(aluno => { //await necessario para o nome carregar sempre mesmo que o utilizador nao retorne para tras da forma correta
+            aluno_nome = aluno[0].nome_aluno;
+            console.log(aluno_nome);
+        })
+
+        buscaRelatoriosDoAlunoX(req.params.aluno).then(relatorios => {
+            //console.log(relatorios);
+            res.render('relatorios', { title: 'Relatórios', relatorios: relatorios, aluno: aluno_nome });
+        })
+    } else {
+        res.redirect('/');
+    }
+})
+
+router.get('/D/:turma/:aluno/relatorios/new', async function(req, res, next) {
+    if (req.session.cookie.secure == true && req.session.cookie.type == 'docente') {
+        res.render('new_relatorio', { title: 'Novo Relatório', turma: req.params.turma, aluno: req.params.aluno});
+    }
+})
+
+/* router.get('/D/:turma/:aluno/mensagens', async function(req, res, next) {
+    if (req.session.cookie.secure == true && req.session.cookie.type == 'docente') {
+        buscaMensagensDoAlunoX(req.params.aluno).then(mensagens => {
+            //console.log(mensagens);
+            res.render('mensagens', { title: 'Mensagens', mensagens: mensagens });
+        })
+    } else {
+        res.redirect('/');
+    }
+}) */
 
 module.exports = router;
