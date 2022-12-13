79ee9ffb9b6bdb4e343cf9040a9ac2f432fa25d7
Eri <ericarodrigues884@gmail.com>
2022-12-13 18:23:25 +0000

    well check no form pls

--- public/javascripts/sendRelatorio.js
+++ public/javascripts/sendRelatorio.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
         });
 
 });
+
+(function() {
+    const headings = document.querySelectorAll('h2');
+    
+    Array.prototype.forEach.call(headings, h => {
+      let btn = h.querySelector('button');
+      let target = h.nextElementSibling;
+      
+      btn.onclick = () => {
+        let expanded = btn.getAttribute('aria-expanded') === 'true';
+        
+        btn.setAttribute('aria-expanded', !expanded);
+        target.hidden = expanded;  
+      }
+    });
+  })()
